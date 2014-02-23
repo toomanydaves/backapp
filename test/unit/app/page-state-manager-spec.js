68d2605a1dae9c44b951cc2d798e6bfe7b833e90
Sean Jacke <seanineurope@gmail.com>
2014-02-23 20:47:13 +0100

    #2 #time 20m Changed references from location to state

--- test/unit/app/page-state-manager-spec.js
+++ test/unit/app/page-state-manager-spec.js
@@ -9,19 +9,19 @@
     var stub = function () { return sandbox.stub(); };
     var reset = function () { sandbox.reset(); };
 
-    describe('pageManager', function () {
-        var pageLocation;
-        var pageManager;
+    describe('pageStateManager', function () {
+        var pageState;
+        var pageStateManager;
 
         before(function (done) {
             browser.call(this, void 0, function () {
-                requirejs([ 'backbone', 'app/framework/PageManager' ], function (Backbone, PageManager) {
-                    pageLocation = _.extend(
-                        { getUrl: stub(), get: stub(), set: stub() },
+                requirejs([ 'backbone', 'app/PageStateManager' ], function (Backbone, PageStateManager) {
+                    pageState = _.extend(
+                        { get: stub(), set: stub(), getUrl: stub() },
                         Backbone.Events
                     );
 
-                    pageManager = new PageManager({ showPage: stub(), pageLocation: pageLocation });
+                    pageStateManager = new PageStateManager({ showPage: stub(), pageState: pageState });
 
                     done();
                 });
@@ -30,15 +30,15 @@
 
         describe('actualize()', function () {
             before(function () {
-                stub(pageManager, '_retrieveState');
+                stub(pageStateManager, '_loadState');
             });
 
-            describe('_pageLocation', function () {
+            describe('_pageState', function () {
                 describe('get()', function () {
-                    it('should get the current location', function () {
-                        pageManager.actualize();
+                    it('should get the current state', function () {
+                        pageStateManager.actualize();
 
-                        expect(pageLocation.get).to.have.been.called;
+                        expect(pageState.get).to.have.been.called;
                     });
     
                     after(reset);
@@ -46,9 +46,9 @@
             });
 
             describe('_actualizeState()', function () {
-                describe('_retrieveState()', function () {
-                    it('should retrieve the state for the current location', function () {
-                        pageLocation.get.returns('foo');
+                describe('_loadState()', function () {
+                    it('should load the current state', function () {
+                        pageState.get.returns('foo');
 
                         pageManager.actualize();
 
@@ -63,7 +63,7 @@
                 var page = { url: 'foo' };
 
                 before(function () {
-                    pageManager._retrieveState.returns(page);
+                    pageStateManager._loadState.returns(page);
                 });
 
                 it('should transition to the actualized state', function () {
@@ -77,24 +77,24 @@
         });
 
         describe('setPage()', function () {
-            describe('_pageLocation', function () {
+            describe('_pageState', function () {
                 describe('set()', function () {
-                    var onLocationChange = spy();
+                    var onStateChange = spy();
 
                     before(function () {
-                        pageManager.on('change:location', onLocationChange);
+                        pageStateManager.on('change:state', onStateChange);
                     });
 
                     it('should set the page location', function () {
-                        pageManager.setPage({ url: 'foo' });
+                        pageStateManager.setPage({ url: 'foo' });
 
-                        expect(pageLocation.set).to.have.been.calledWith('foo');
+                        expect(pageState.set).to.have.been.calledWith('foo');
                     });
 
-                    it('should trigger a location change event', function () {
-                        pageLocation.trigger('change', 'bar');
+                    it('should trigger a state change event', function () {
+                        pageState.trigger('change', 'bar');
 
-                        expect(onLocationChange).to.have.been.calledWith('bar', pageManager);
+                        expect(onStateChange).to.have.been.calledWith('bar', pageStateManager);
                     });
 
                     afterEach(reset);
@@ -105,9 +105,9 @@
                 var page = { url: 'foo' };
 
                 it('should transition to the set page', function () {
-                    pageManager.setPage(page);
+                    pageStateManager.setPage(page);
 
-                    expect(pageManager._transition).to.have.been.calledWith(page)
+                    expect(pageStateManager._transition).to.have.been.calledWith(page)
                 });
             });
         });
@@ -126,9 +126,9 @@
 
             describe('showPage()', function () {
                 it('should show the given state', function () {
-                    pageManager._transition(page);
+                    pageStateManager._transition(page);
 
-                    expect(pageManager.showPage).to.have.been.calledWith(page);
+                    expect(pageStateManager.showPage).to.have.been.calledWith(page);
                 });
 
                 after(reset);
@@ -138,21 +138,21 @@
                 var deferred;
 
                 before(function () {
-                    stub(pageManager, '_removePage');
+                    stub(pageStateManager, '_removePage');
 
                     deferred = new $.Deferred();
 
-                    pageManager.showPage.returns(deferred);
+                    pageStateManager.showPage.returns(deferred);
                 });
 
                 it('should not remove a previous page before it is finished showing the current one', function () {
-                    pageManager._transition(page);
+                    pageStateManager._transition(page);
 
-                    expect(pageManager._removePage).to.not.have.been.called;
+                    expect(pageStateManager._removePage).to.not.have.been.called;
 
                     deferred.resolve();
 
-                    expect(pageManager._removePage).to.have.been.called;
+                    expect(pageStateManager._removePage).to.have.been.called;
                 });
 
                 after(reset);
@@ -162,21 +162,21 @@
                 var deferred;
 
                 before(function () {
-                    stub(pageManager, '_addPage');
+                    stub(pageStateManager, '_addPage');
 
                     deferred = new $.Deferred();
 
-                    pageManager.showPage.returns(deferred);
+                    pageStateManager.showPage.returns(deferred);
                 });
 
                 it('should not add the current page before it is finished showing it', function () {
-                    pageManager._transition(page);
+                    pageStateManager._transition(page);
 
-                    expect(pageManager._addPage).to.not.have.been.called;
+                    expect(pageStateManager._addPage).to.not.have.been.called;
 
                     deferred.resolve();
 
-                    expect(pageManager._addPage).to.have.been.calledWith(page);
+                    expect(pageStateManager._addPage).to.have.been.calledWith(page);
                 });
             });
         });
