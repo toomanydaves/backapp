b84bb885d7e9b412610a175f90c2b1f122236170
Sean Jacke <seanineurope@gmail.com>
2014-02-25 21:58:35 +0100

    #14 #time 5m Added basic spec setup to outline

--- test/unit/app/page-viewer-spec.js
+++ test/unit/app/page-viewer-spec.js
@@ -1,7 +1,27 @@
 (function (require, describe, it) {
     'use strict';
 
+    var config = require('../../../setup/config');
+    var amd = config.amd;
+    var browser = config.browser;
+    var expect = config.expect;
+    var sandbox = config.sandbox.create();
+    var spy = function (object, method) { return sandbox.spy(object, method); };
+    var stub = function (object, method) { return sandbox.stub(object, method); };
+    var reset = function () { sandbox.reset(); };
+    var modules = { };
+
     describe('pageViewer', function () {
-        describe('showPage()', function () { });
+        before(function (done) {
+            browser.call(this, void 0, function () {
+                amd([ 'app/PageViewer' ], function (PageViewer) {
+                    modules.PageViewer = PageViewer;
+
+                    done();
+                });
+            });
+        });
+
+        describe('showPage()', function () {});
     });
-})(require, describe, it);
+}(require, describe, it));
